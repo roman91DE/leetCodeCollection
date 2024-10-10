bd724afaac07fe11203e2c80d1c6033e48b73797
Roman Hoehn <rohoehn123@gmail.com>
2024-10-10 22:06:35 +0200

    solved 121 using kardanes algorithm
    Signed-off-by: Roman Hoehn <rohoehn123@gmail.com>

--- top_interview_150/121_best_time_buy_sell_stocks/main.go
+++ top_interview_150/121_best_time_buy_sell_stocks/main.go
@@ -4,24 +4,61 @@ import (
 	"fmt"
 )
 
+// my first implementation brute forces all combinations and is 
+// therefore too slow on large test cases
+
+// func maxProfit(prices []int) int {
+// 	profit, temp := 0, 0
+// 	for buyIdx, buyPrice := range prices {
+// 		for _, sellPrice := range prices[buyIdx:] {
+// 			if sellPrice > buyPrice {
+// 				temp = sellPrice - buyPrice
+// 				if temp > profit {
+// 					profit = temp
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return profit
+// }
+
+
 // maxProfit function calculates the maximum profit that can be achieved
 func maxProfit(prices []int) int {
 
-	profit, temp := 0, 0
-
-	for buyIdx, buyPrice := range prices {
-		for _, sellPrice := range prices[buyIdx:] {
-			if sellPrice > buyPrice {
-				temp = sellPrice - buyPrice
-				if temp > profit {
-					profit = temp
-				}
-			}
-
-		}
+	if len(prices) <= 1{
+		return 0
 	}
-	return profit
+
+	// idea: transform array to price changes relative to the previous price
+	priceDiffs := make([]int, len(prices))
+	priceDiffs[0] = 0
+
+	for i, val := range prices[1:] {
+		priceDiffs[i+1] = val - prices[i]
+	}
+
+	// kardane's algorithm finds the largest sum over all contigous sub-arrays
+	return kardaneAlgorithm(priceDiffs)
+
 }
+
+// kardane's algorithm finds the largest sum of a contigous subarray
+func kardaneAlgorithm(diffs []int) int {
+	// 0 also reflects negative returns in the challenge
+	bestSum := 0
+	currentSum := 0
+
+	for _, val := range diffs {
+		// current sum is either the current value or the previous rolling sum + the current value
+		currentSum = max(val, currentSum+val)
+		// save best results 
+		bestSum = max(bestSum, currentSum)
+	}
+	return bestSum
+}
+
+
 
 func main() {
 	// Test cases
@@ -34,7 +71,9 @@ func main() {
 		{[]int{1, 2, 3, 4, 5}, 4}, // Increasing prices
 		{[]int{5, 4, 3, 2, 1}, 0}, // Decreasing prices
 		{[]int{3, 3, 3, 3, 3}, 0}, // All prices are the same
-		{[]int{}, 0},              // Empty array
+		{[]int{}, 0},	          // Empty array
+		{[]int{2,4,1}, 2},
+		{[]int{2,11,1,4,7}, 9},
 	}
 
 	for i, testCase := range testCases {
